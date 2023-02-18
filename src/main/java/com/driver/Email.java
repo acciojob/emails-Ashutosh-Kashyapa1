4f60662f93cb6aafea030319b717328fd964fade
Ashutosh-Kashyapa1 <ashutosh1998iic@gmail.com>
2023-02-18 22:37:15 +0530

    pass

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,6 +5,7 @@ public class Email {
     private String emailId;
     private String password;
 
+
     public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
@@ -25,5 +26,33 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(this.password.equals(oldPassword))
+        {
+            if(newPassword.length()>=8)
+            {
+                int up=0;
+                int lo=0;
+                int digit=0;
+                int sp=0;
+                for(int i =0 ; i <newPassword.length();i++)
+                {
+                    char ch =newPassword.charAt(i);
+                    if(ch >='A' && ch <='Z')
+                        up++;
+                    else if(ch>='a' && ch<='z')
+                        lo++;
+                    else if(ch>='0' && ch <='9')
+                        digit++;
+                    else
+                        sp++;
+                }
+                if(up >=1 && lo>=1 && digit >=1 && sp>=1)
+                {
+                    this.password=newPassword;
+                }
+
+            }
+
+        }
     }
 }
